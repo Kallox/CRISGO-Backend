846b77089688f41247235611f0ee2edae95e193b
Kallox <d34d.mc@gmail.com>
2025-02-11 21:30:04 -0300

    Feat[Basic API]: Endpoint to get app info using Gin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CRIS-Backend
+
+To Run:
+
+1. go mod tidy
+2. air
+3. go to localhost:8080
---
+++ cmd/server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/ini.v1"
+)
+
+// getConfig - Read the server configuration file and return the values
+func getConfig() map[string]string {
+	var config = make(map[string]string)
+	cfg, err := ini.Load("config/server.cfg")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v\n", err)
+		return nil
+	}
+	config["appName"] = cfg.Section("server").Key("app_name").String()
+	config["appDescription"] = cfg.Section("server").Key("app_description").String()
+	config["contact"] = cfg.Section("server").Key("contact").String()
+	config["version"] = cfg.Section("server").Key("version").String()
+
+	return config
+}
+
+// setupRouter - Setup the routes for the application
+func setupRouter() *gin.Engine {
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Main route to get the status of the application
+	r.GET("/", func(c *gin.Context) {
+		config := getConfig()
+		c.JSON(http.StatusOK, config)
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
