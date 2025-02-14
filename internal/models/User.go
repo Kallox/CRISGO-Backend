9150bac830521b69242d7db74cd9e5423433b594
Kallox <d34d.mc@gmail.com>
2025-02-14 19:22:55 -0300

    Fix[Unused ID]: Removed ID field from user model

--- internal/models/User.go
+++ internal/models/User.go
@@ -5,7 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 
-	ID       uint   `gorm:"primaryKey autoIncrement" json:"id"`
 	Username string `gorm:"not null " json:"username"`
 	Fullname string `gorm:"not null" json:"fullname"`
 	Password string `gorm:"not null" json:"password"`
